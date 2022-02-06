f4b1feb4b137cb7c97a45719f85c17249bbcd454
Vadim Semenov <5587394@mail.ru>
2022-02-06 16:04:28 +0300

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,32 @@
+package ru.job4j.oop;
+
+public class Error {
+    private boolean active;
+    private int status;
+    private String message;
+
+    public Error() {
+        this.active = true;
+        this.status = 1;
+        this.message = "Error";
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void printInfo() {
+        System.out.println(this.active + " " + this.status + " " + this.message);
+    }
+
+    public static void main(String[] args) {
+        Error error = new Error();
+        Error error1 = new Error(true, 2, "OutOfRange");
+        Error error2 = new Error(true, 3, "Unknown name");
+        error.printInfo();
+        error1.printInfo();
+        error2.printInfo();
+    }
+}
