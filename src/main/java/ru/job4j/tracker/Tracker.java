ecfa5abc30ee0169b335020862be1b82eafd85f6
Vadim Semenov <5587394@mail.ru>
2022-02-07 20:10:06 +0300

    7. Метод удаления заявки Tracker.delete [#211749 #261379]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -48,11 +48,9 @@ public class Tracker {
     public boolean delete(int id) {
         int index = indexOf(id);
         if (index != -1) {
-            items[index] = null;
+            System.arraycopy(items, index + 1, items, index, size - index - 1);
+            items[size - 1] = null;
             size--;
-            if (size > 1) {
-                System.arraycopy(items, index + 1, items, index, size - index);
-            }
             return true;
         }
         return false;
